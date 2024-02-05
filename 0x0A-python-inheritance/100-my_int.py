63e6612f7e2b55b460b3aa46d68f9a0f4c932090
Sadow0x <maaroufofficiel@gmail.com>
2024-02-05 20:14:16 +0100

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains the class MyInt
+"""
+
+
+class MyInt(int):
+    """rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+ 
