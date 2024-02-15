8a223c3cb0261e5795962350969f6152af3778b9
Sadow0x <maaroufofficiel@gmail.com>
2024-02-15 19:40:11 +0100

    Update 18-main.py

--- 0x0C-python-almost_a_circle/18-main.py
+++ 0x0C-python-almost_a_circle/18-main.py
@@ -30,3 +30,13 @@ if __name__ == "__main__":
 
     Square.save_to_file(list_squares_input)
 
+    list_squares_output = Square.load_from_file()
+
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
+
